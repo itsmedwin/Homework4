8c42942367a97af85d3b374b03ebaa91aa11b5d8
itsmedwin <edwinb@mail.sfsu.edu>
2015-04-03 15:29:49 -0700

    Create account.h

---
+++ account.h
@@ -0,0 +1,23 @@
+#ifndef __Homework5Trial__account__
+#define __Homework5Trial__account__
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Account
+{
+ 
+public:
+ Account account;
+ 
+ int id;
+ string title;
+ double balance;
+ 
+ 
+};
+
+#endif /* defined(__Homework5Trial__account__) */

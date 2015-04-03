5c7052c36d7457301e94ce461942286b78e3e2b6
itsmedwin <edwinb@mail.sfsu.edu>
2015-04-03 15:30:43 -0700

    Create envelope.h

---
+++ envelope.h
@@ -0,0 +1,18 @@
+#ifndef __Homework5Trial__envelope__
+#define __Homework5Trial__envelope__
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Envelope
+{
+ 
+ int id;
+ string title;
+ 
+};
+
+#endif /* defined(__Homework5Trial__envelope__) */
